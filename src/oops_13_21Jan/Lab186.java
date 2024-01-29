c763fa068f52f11e18a48925fca2228fe0462078
SheetalNagmode <sheetalnagmode@gmail.com>
2024-01-29 13:43:21 -0500

    OOPs_13_(21 Jan 2024)

---
+++ src/oops_13_21Jan/Lab186.java
@@ -0,0 +1,13 @@
+package src.oops_13_21Jan;
+
+import java.util.List;
+
+public class Lab186 {
+    public static void main(String[] args) {
+
+        List stringList = null;
+        stringList.add(200);
+        // NullList can't have a values
+
+    }
+}
